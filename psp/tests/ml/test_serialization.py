6ccc3923a209d529929aee2d89a7bc4d800a0711
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2023-02-10 16:01:09 +0000

    Remove warning in tests

--- psp/tests/ml/test_serialization.py
+++ psp/tests/ml/test_serialization.py
@@ -4,13 +4,13 @@ from psp.ml.serialization import load_model, save_model
 
 # Inheriting from some model to not have to implement all the methods like we would have to do if we
 # defined one from scratch.
-class TestModel(YesterdayPvSiteModel):
+class _TestModel(YesterdayPvSiteModel):
     def __init__(self):
         pass
 
 
 def test_save_and_load(tmp_path):
-    model = TestModel()
+    model = _TestModel()
     path = tmp_path / "save_and_load.pkl"
     save_model(model, path)
     model2 = load_model(path)
