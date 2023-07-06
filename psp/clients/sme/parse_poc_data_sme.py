0273f337a8353fd80e4b71100f686ae8d4f5fa0e
Megawattz <zakjrwatts@gmail.com>
2023-07-06 15:32:51 +0000

    add lat long

--- psp/clients/sme/parse_poc_data_sme.py
+++ psp/clients/sme/parse_poc_data_sme.py
@@ -20,6 +20,7 @@ def main():
     dfs = [pd.read_csv(x, parse_dates=["UtcDatetime"]) for x in csv_files]
 
     df = pd.concat(dfs, ignore_index=True)    
+
     ds = xr.Dataset.from_dataframe(df)
     ds = ds.set_index(index=['UtcDatetime', 'SiteId']).unstack('index')
     
@@ -34,25 +35,25 @@ def main():
 
     # Add the lat/lon that we googled for each location in the provided Location.txt.
     city = pd.DataFrame(index=names, columns=["lat", "lon"])
-    city.loc["4353"][["lat", "lon"]] = (51.2869, -0.7526)
-    city.loc["4403"][["lat", "lon"]] = (51.5072, 0.1276)
-    city.loc["4416"][["lat", "lon"]] = (52.6383, 1.5506)
-    city.loc["4417"][["lat", "lon"]] = (52.3024, 0.6940)
-    city.loc["4423"][["lat", "lon"]] = (55.7832, -3.9811)
-    city.loc["6331"][["lat", "lon"]] = (53.8008, -1.5491)
-    city.loc["6332"][["lat", "lon"]] = (51.45000076, -1.72939467)
-    city.loc["6378"][["lat", "lon"]] = (50.8376, -0.7749)
-    city.loc["6381"][["lat", "lon"]] = (51.2869, -0.7526)
-    city.loc["6385"][["lat", "lon"]] = (51.5072, 0.1276)
-    city.loc["6590"][["lat", "lon"]] = (52.6383, 1.5506)
-    city.loc["6592"][["lat", "lon"]] = (52.3024, 0.6940)
-    city.loc["6594"][["lat", "lon"]] = (55.7832, -3.9811)
-    city.loc["6596"][["lat", "lon"]] = (53.8008, -1.5491)
-    city.loc["6600"][["lat", "lon"]] = (51.45000076, -1.72939467)
-    city.loc["6605"][["lat", "lon"]] = (50.8376, -0.7749)
-    city.loc["6670"][["lat", "lon"]] = (51.45000076, -1.72939467)
-    city.loc["25842"][["lat", "lon"]] = (50.8376, -0.7749)
-    city.loc["27855"][["lat", "lon"]] = (50.8376, -0.7749)
+    city.loc["4353"][["lat", "lon"]] = (51.11765, 0.93344)
+    city.loc["4403"][["lat", "lon"]] = (51.65856, -1.79106)
+    city.loc["4416"][["lat", "lon"]] = (50.79061, -1.85936)
+    city.loc["4417"][["lat", "lon"]] = (50.79061, -1.85936)
+    city.loc["4423"][["lat", "lon"]] = (51.64675, -1.80934)
+    city.loc["6331"][["lat", "lon"]] = (51.97173, 0.36421)
+    city.loc["6332"][["lat", "lon"]] = (51.82652, 0.72746)
+    city.loc["6378"][["lat", "lon"]] = (52.61083, -1.56508)
+    city.loc["6381"][["lat", "lon"]] = (52.21649, -1.4398)
+    city.loc["6385"][["lat", "lon"]] = (53.02505, -0.73305)
+    city.loc["6590"][["lat", "lon"]] = (51.44815, -1.98928)
+    city.loc["6592"][["lat", "lon"]] = (50.85989, -0.7939)
+    city.loc["6594"][["lat", "lon"]] = (51.75541, -1.64461)
+    city.loc["6596"][["lat", "lon"]] = (51.63601, -1.39277)
+    city.loc["6600"][["lat", "lon"]] = (51.48668, -1.559777)
+    city.loc["6605"][["lat", "lon"]] = (51.38238, -2.0898)
+    city.loc["6670"][["lat", "lon"]] = (50.82233, -4.42377)
+    city.loc["25842"][["lat", "lon"]] = (51.75541, -1.64461)
+    city.loc["27855"][["lat", "lon"]] = (51.65529, -1.16366)
 
     # Make lists of lat and lon that matches the order of pv_id in the data array.
     lat = [city.loc[SiteId, "lat"] for SiteId in site_ids]
@@ -65,6 +66,14 @@ def main():
     ds = ds.drop_vars(["Unnamed: 0", "PostCode"])
     ds = ds.rename({"UtcDatetime": "ts", "SiteId": "pv_id"})
     ds = ds.rename({"Actual_MW": "power", "CapacityMw": "capacity"})
+
+
+    # Drop any NaNs
+
+    # ds = ds.dropna(dim="ts", subset=["power"])
+
+    ds = ds.dropna('ts', how='all')
+    
 
     # Save!
     ds.to_netcdf(args.output)
