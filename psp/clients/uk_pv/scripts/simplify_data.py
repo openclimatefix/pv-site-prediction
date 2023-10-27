f0437158cfef71e96f4cc6bc6632a2e22255d2bb
Megawattz <zakjrwatts@gmail.com>
2023-10-27 12:07:32 +0000

    reformat uk_pv script

--- psp/clients/uk_pv/scripts/simplify_data.py
+++ psp/clients/uk_pv/scripts/simplify_data.py
@@ -58,7 +58,7 @@ def main():
 
         dfs[f"{n}"] = df.loc[ss_n]
 
-        dfs[f"{n}_1M"] = dfs[f"{n}"].sample(1_000_000, random_state=rnd_state) 
+        dfs[f"{n}_1M"] = dfs[f"{n}"].sample(1_000_000, random_state=rnd_state)
         dfs[f"{n}_10k"] = dfs[f"{n}_1M"].sample(10_000, random_state=rnd_state)
 
         # Glasgow region.
