f37b7d3a284e99a65837db54dfdc4f9e218a5a28
peterdudfield <peter.dudfield@hotmail.com>
2023-12-04 20:33:54 +0000

    fix test

--- psp/tests/data/data_sources/test_satellite_data_source.py
+++ psp/tests/data/data_sources/test_satellite_data_source.py
@@ -1,23 +1,21 @@
-
 import xarray as xr
 
 from psp.data_sources.satellite import SatelliteDataSource
 
-import ocf_blosc2
-
-
 
 def test_satellite_data_source():
-    """ Test loading the satellite data
+    """Test loading the satellite data
 
     Note this test uses the satellite public dataset to get this data.
 
     """
-    paths = ['gs://public-datasets-eumetsat-solar-forecasting/satellite/EUMETSAT/SEVIRI_RSS/v4/2021_nonhrv.zarr']
+    paths = [
+        "gs://public-datasets-eumetsat-solar-forecasting/satellite/EUMETSAT/SEVIRI_RSS/v4/2021_nonhrv.zarr"
+    ]
 
-    ds = xr.open_zarr(
+    xr.open_zarr(
         "gs://public-datasets-eumetsat-solar-forecasting/satellite/EUMETSAT/SEVIRI_RSS/v4/2022_nonhrv.zarr",
         consolidated=True,
-
+    )
 
     _ = SatelliteDataSource(paths_or_data=paths)
