bebbf7dc8a0038d9d891e74739ad29d7fd4d91aa
peterdudfield <peter.dudfield@hotmail.com>
2023-12-04 21:06:47 +0000

    tidy

--- psp/data_sources/satellite.py
+++ psp/data_sources/satellite.py
@@ -4,5 +4,4 @@ from psp.data_sources.nwp import NwpDataSource
 class SatelliteDataSource(NwpDataSource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._data = None
-        self._transformer = None
+
