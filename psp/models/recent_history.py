45035ac1e6139aef68486bf515035fb136c09fd0
peterdudfield <peter.dudfield@hotmail.com>
2024-02-20 08:30:30 +0000

    add len

--- psp/models/recent_history.py
+++ psp/models/recent_history.py
@@ -535,14 +535,14 @@ class RecentHistoryModel(PvSiteModel):
         # make sure recent power values is the right length
         if not hasattr(self, "_n_recent_power_values"):
             self._n_recent_power_values = 0
-        if recent_power_values < self._n_recent_power_values:
+        if len(recent_power_values) < self._n_recent_power_values:
             recent_power_values = np.pad(
                 recent_power_values,
                 (0, self._n_recent_power_values - len(recent_power_values)),
                 "constant",
                 constant_values=np.nan,
             )
-        elif recent_power_values > self._n_recent_power_values:
+        elif len(recent_power_values) > self._n_recent_power_values:
             recent_power_values = recent_power_values[-self._n_recent_power_values :]
 
         if self._normalize_features:
